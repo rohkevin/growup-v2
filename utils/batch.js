789c60e86c6630cb2f92be4dab187df3ec610080
rohkevin <kevinrohmail@gmail.com>
2021-05-18 09:27:28 -0400

    Change utils to get env variables for firebase configuration

--- utils/batch.js
+++ utils/batch.js
@@ -4,9 +4,13 @@ const firebase = require("firebase");
 require("firebase/firestore");
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDF3Yw4L_5odWGXNNJUDYUCPqPSYf7OgcI",
-    authDomain: "growupv2.firebaseapp.com",
-    projectId: "growupv2"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID 
   });
   
 var db = firebase.firestore();
--- utils/uploader.js
+++ utils/uploader.js
@@ -4,9 +4,13 @@ require("firebase/firestore");
 
 // Initialize Cloud Firestore through Firebase
 firebase.initializeApp({
-    apiKey: "AIzaSyDF3Yw4L_5odWGXNNJUDYUCPqPSYf7OgcI",
-    authDomain: "growupv2.firebaseapp.com",
-    projectId: "growupv2"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID 
   });
   
 var db = firebase.firestore();
@@ -193,7 +197,7 @@ var blogPostsJSON = [
 ]
 
 blogPostsJSON.forEach(function(obj) {
-    newId = obj.id;
+    const newId = obj.id;
     console.log(newId);
     console.log(typeof newId);
     db.collection("posts").doc(newId).set({
